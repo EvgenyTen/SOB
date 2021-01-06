4724bd8ab3aec1096ac5f2e252df9a1085f6ab8e
EvgenyT <evgeny.tenenbaum@gmail.com>
2021-01-06 12:28:14 +0200

    enter emulation

---
+++ src/main/java/Start.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Start {
+    public static void main(String [] args){
+        Scanner scanner=new Scanner(System.in);
+        int choise;
+        while(true){
+            System.out.println("For Client enter emulation ,Please Enter -1");
+            System.out.println("For Worker enter emulation ,Please Enter -2");
+            System.out.println("For Quit ,Please Enter -3");
+            System.out.println("Enter your Choise:   ");
+            choise=scanner.nextInt();
+            
+        }
+    }
+}
