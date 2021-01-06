f1f6b25188b7550d8c310755f461972cd6647275
EvgenyT <evgeny.tenenbaum@gmail.com>
2021-01-06 19:23:18 +0200

    +lombok

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,14 +1,9 @@
 import AuthorizationClasses.AuthorizationClass;
-import AuthorizationClasses.Client;
-import AuthorizationClasses.Worker;
 
 public class Start {
     public static void main(String[] args) {
         AuthorizationClass.entrance();
-        Worker onew=new Worker("Ivan11","dadada");
-        Worker twow=new Worker("Ivan12","netnet");
-        Client onec = new Client(1234567890123456,1111,1000000);
-        Client twoc = new Client(2222222222222222,2222,50000);
+
             }
         }
 
